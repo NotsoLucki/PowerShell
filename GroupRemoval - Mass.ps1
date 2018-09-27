9ae0505ea97bc1ee4d790ffb030613f2b0056409
NotsoLucki <ethodges3@gmail.com>
2018-09-27 11:27:02 -0400

    Update GroupRemoval - Mass.ps1

--- GroupRemoval - Mass.ps1
+++ GroupRemoval - Mass.ps1
@@ -1,1 +1,4 @@
+#This PS script will remove GroupName from all Computers listed in the CSV file.
+#Whatever is listed for -Header will be used for -Members (preceeding $_.)
+
 Import-Csv -Path drive:\Path\of\the\File.csv -Header ColumnNametobeImported | ForEach-Object {Remove-ADGroupMember -Identity GroupName -Members $_.ColumnNametobeImported}
