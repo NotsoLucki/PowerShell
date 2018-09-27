1f92affb7636e02e2edc8e99cf6604ad89612ffa
NotsoLucki <ethodges3@gmail.com>
2018-09-27 10:31:57 -0400

    Update GroupRemoval.ps1

--- GroupRemoval.ps1
+++ GroupRemoval.ps1
@@ -1,1 +1,6 @@
-﻿Remove-ADGroupMember -Identity GroupName -Members UserName, ComputerName$ -whatif
+#This PS script will remove GroupName from UserName and ComputerName$. 
+#To add more than one user or computer, use a comma. 
+#Computers need a $ at the end of it. 
+#The -whatif is used to see if the script will work; remove it to run the script.
+
+Remove-ADGroupMember -Identity GroupName -Members UserName, ComputerName$ -whatif
